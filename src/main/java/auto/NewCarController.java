9a54f875a63a7af4181e8a5ffd2eb8cbcefbfa35
Csontos Róbert <nagyrobert1996@gmail.com>
2020-09-20 20:03:40 +0200

    Új autó controller mező változók hozzáadása

--- src/main/java/auto/NewCarController.java
+++ src/main/java/auto/NewCarController.java
@@ -1,10 +1,30 @@
 package auto;
 
-
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class NewCarController {
 
-    public void saveNewCarToDatabase(MouseEvent mouseEvent) {
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField phoneNumber;
+
+    @FXML
+    private TextField brandAndType;
+
+    @FXML
+    private TextField licencePlate;
+
+    @FXML
+    private TextArea description;
+
+    @FXML
+    void saveNewCarToDatabase(MouseEvent event) {
+
     }
+
 }
