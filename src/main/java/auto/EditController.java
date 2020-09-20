85e546892447e72afed8a81aa0bc3e5068a7a20c
Csontos Róbert <nagyrobert1996@gmail.com>
2020-09-20 20:08:46 +0200

    Szerkesztés controller osztály változók hozzáadása

--- src/main/java/auto/EditController.java
+++ src/main/java/auto/EditController.java
@@ -1,10 +1,34 @@
 package auto;
 
-
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class EditController {
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField phoneNumber;
+
+    @FXML
+    private TextField brandAndType;
+
+    @FXML
+    private TextField licencePlane;
+
+    @FXML
+    private TextArea description;
+
+    @FXML
+    private Spinner<?> cost;
+
+    @FXML
+    private ComboBox<?> state;
 
     @FXML
     void save(MouseEvent event) {
