7cf38e1f20e97f4d86631764eefbe27c0f24dc21
Csontos Róbert <nagyrobert1996@gmail.com>
2020-09-20 20:20:33 +0200

    Lista fx:id megadása a kész autók layouton

--- src/main/java/auto/DoneController.java
+++ src/main/java/auto/DoneController.java
@@ -1,10 +1,14 @@
 package auto;
 
-
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import models.Car;
 
 public class DoneController {
+
+    @FXML
+    private ListView<Car> listOfCarsDone;
 
     @FXML
     void setPaidStatus(MouseEvent event) {
