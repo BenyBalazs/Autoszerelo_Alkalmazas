1fbc5aa491cc390faf3564785e2df3d94f4f9f4c
Csontos Róbert <nagyrobert1996@gmail.com>
2020-09-20 10:27:31 +0200

    Menu layout elkészítése

---
+++ src/main/java/auto/MenuController.java
@@ -0,0 +1,23 @@
+package auto;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class MenuController {
+
+    @FXML
+    void done(MouseEvent event) {
+
+    }
+
+    @FXML
+    void new_car(MouseEvent event) {
+
+    }
+
+    @FXML
+    void repair(MouseEvent event) {
+
+    }
+
+}
