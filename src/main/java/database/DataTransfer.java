a38edc20ce89462e3899714e6bb8192d4235d2d2
Patrik Kis <kisp13@gmail.com>
2020-09-26 14:03:46 +0200

    Data Transfer osztály megírása

--- src/main/java/database/DataTransfer.java
+++ src/main/java/database/DataTransfer.java
@@ -1,4 +1,21 @@
 package database;
 
-public class DataTransfer {
+import models.Car;
+
+public final class DataTransfer {
+
+    private static Car car;
+
+    private DataTransfer(){ }
+
+    public static Car getCar(){
+
+        return car;
+    }
+
+    public static void setCar(Car paramCar){
+
+        car = paramCar;
+    }
 }
+
