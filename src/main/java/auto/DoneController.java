b1d7ee1e316d30a6ffe5f54f010fc611a0551526
Patrik Kis <kisp13@gmail.com>
2020-09-26 16:02:42 +0200

    Done Kontroller megírása

--- src/main/java/auto/DoneController.java
+++ src/main/java/auto/DoneController.java
@@ -1,7 +1,9 @@
 package auto;
 
+import database.Database;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.input.MouseEvent;
 import models.Car;
 
@@ -11,8 +13,17 @@ public class DoneController {
     private ListView<Car> listOfCarsDone;
 
     @FXML
-    void setPaidStatus(MouseEvent event) {
+    public void initialize(){
+        listOfCarsDone.getItems().clear();
+        listOfCarsDone.getItems().addAll(Database.getAllCarsByState(Car.State.FINISHED));
+        listOfCarsDone.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
 
+    @FXML
+    void setPaidStatus(MouseEvent event) {
+        Car car = listOfCarsDone.getSelectionModel().getSelectedItem();
+        car.setState(Car.State.PAID);
+        Database.commitCarChanges(car);
     }
 
 }
